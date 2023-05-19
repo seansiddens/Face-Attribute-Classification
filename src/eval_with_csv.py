da32896d74f40b1ce83f34bc9d8a7dbce0fb483e
Hao Tran <haoanhtran7@gmail.com>
2023-05-19 15:54:11 -0700

    edit: functional code

--- src/eval_with_csv.py
+++ src/eval_with_csv.py
@@ -1,28 +1,9 @@
 import csv
 
 ATTRIBUTES = "main_class sub_class img_id 5_o_Clock_Shadow Arched_Eyebrows Attractive Bags_Under_Eyes Bald Bangs Big_Lips Big_Nose Black_Hair Blond_Hair Blurry Brown_Hair Bushy_Eyebrows Chubby Double_Chin Eyeglasses Goatee Gray_Hair Heavy_Makeup High_Cheekbones Male Mouth_Slightly_Open Mustache Narrow_Eyes No_Beard Oval_Face Pale_Skin Pointy_Nose Receding_Hairline Rosy_Cheeks Sideburns Smiling Straight_Hair Wavy_Hair Wearing_Earrings Wearing_Hat Wearing_Lipstick Wearing_Necklace Wearing_Necktie Young".split()
+THRESHOLD = 0.5
 
-def convertToBinaryPred(): 
-    return 0
-
-
-def loadTruthLabels(): 
-    return 0
-
-def main(): 
-    csvFilename = "prediction-v2.csv"
-    outputFilename = "output.txt"
-    truth = {"Blond_Hair": 221, "Male": -221} #input for truth labels
-    pred = {} #hash with binary classifications
-    accuracy = {}
-
-    #load accuracy hash
-    for k in truth.keys(): 
-        accuracy[k] = 0 
-
-    threshold = 0.5
-
-
+def convertToBinaryPred(binaryPredHash, csvFilename, imageNum): 
     csvFile = open(csvFilename, "r")
     csvReader = csv.reader(csvFile)
 
@@ -33,40 +14,33 @@ def main():
             index += 1 
             continue  
 
+        imageNum += 1 
         #populate each image_id with a zero vector 
         #assume each image_id is unique 
         n = len(row)
-        pred[row[2]] = [0] * (n-3)
+        binaryPredHash[row[2]] = [0] * (n-3)
         
         #check each attribute 
         for i in range(3, 43): 
-            if(float(row[i]) > threshold):
-                pred[row[2]][i-3] = 1
+            if(float(row[i]) > THRESHOLD):
+                binaryPredHash[row[2]][i-3] = 1
             else: 
-                pred[row[2]][i-3] = 0
+                binaryPredHash[row[2]][i-3] = 0
 
 
-    # keys = pred.keys()
-    # for k in keys: 
-    #     binaryPred = pred[k]
-    #     n = len(ATTRIBUTES)     
-    #     truthKeys = truth.keys()
-    #     for i in range(n): 
-    #         if ATTRIBUTES[i] in truthKeys: 
-    #             print("here")
-    #             key = ATTRIBUTES[i]
-    #             accuracy[key] += binaryPred[i]
+def loadTruthLabels(truthLabels, accuracy, indexArray, truthLabelArray): 
+    #load accuracy hash
+    for k in truthLabels.keys(): 
+        accuracy[k] = 0 
 
     #find the indexes 
-    indexArray = []
-    truthLabelArray = []
-    truthLabels = truth.keys()
+    truthLabels = truthLabels.keys()
     for label in truthLabels: 
         index = ATTRIBUTES.index(label)
         indexArray.append(index - 3)
         truthLabelArray.append(label)
 
-    #iterate through binary predictions
+def calculateTotalLabels(pred, accuracy, indexArray, truthLabelArray): 
     keys = pred.keys()
     for k in keys: 
         binaryPred = pred[k]
@@ -75,37 +49,39 @@ def main():
             index = indexArray[i]
             label = truthLabelArray[i]
             accuracy[label] += binaryPred[index]
-            
+
+def calculateAccuracy(truth, labelCount, numImages): 
+    return 0
+
+def outputToFile(outputFilename): 
+    return 0
+
+def main(csvFilename, outputFilename, truth): 
+    pred = {} 
+    labelCount = {}
+    indexArray = []
+    truthLabelArray = []
+    imageNum = 0
+
+    loadTruthLabels(truth, labelCount, indexArray, truthLabelArray) 
+    convertToBinaryPred(pred, csvFilename, imageNum)
+    calculateTotalLabels(pred, labelCount, indexArray, truthLabelArray)
+    calculateAccuracy(truth, labelCount, imageNum)
+    #outputToFile(outputFilename)
+
     #output accuracy
-    outputFile = open(outputFilename, "w")
-    for label in truthLabelArray: 
-        rawAcc = accuracy[label] / truth[label]
-        roundedAcc = round(rawAcc, 2)
-        outputString = f"Label: {label} had an accuracy of {roundedAcc}\n"
-        outputFile.write(outputString)
-        
-            
-
-
-
-
-  
-
-
-
-    # with open(csvFilename, "r") as csvFile: 
-    #     csvReader = csv.reader(csvFile)
-
-    #     #convert output to binary classificaiton
-    #     for row in csvReader:
-    #         n = len(row)
-    #         #set vector to 0 
-    #         pred[row[2]] = [0] * (n-3)
-    #         for i in range(3, n): 
-    #             print(row[i][1])
-    #         break
-
-
+    # outputFile = open(outputFilename, "w")
+    # for label in truthLabelArray: 
+    #     rawAcc = accuracy[label] / truth[label]
+    #     roundedAcc = round(rawAcc, 2)
+    #     outputString = f"Label: {label} had an accuracy of {roundedAcc}\n"
+    #     outputFile.write(outputString)
 
 if __name__ == "__main__":
-    main()
+    #INPUTS TO SCRIPT: 
+    csvFilename = "prediction-v2.csv"
+    outputFilename = "blonde-women.txt"
+    truth = {"Blond_Hair": 221, "Male": 0}
+    #----------------------------------------
+
+    main(csvFilename, outputFilename, truth)
